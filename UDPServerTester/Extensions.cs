eb8a499ceb61f91b8b8296c33ae574bc84ce6ed9
Maksim Makushchenko <maksalmak@gmail.com>
2017-03-28 13:48:37 +0300

    Added logging to WEB test project

---
+++ UDPServerTester/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UDPServerTester
+{
+    public static class Extensions
+    {
+        public static async Task SendAsync(this WebSocket socket, string message)
+        {
+            var buffer = new ArraySegment<byte>(Encoding.UTF8.GetBytes(message));
+            await socket.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
+        }
+
+        public static async Task<string> GetMessage(this WebSocket socket)
+        {
+            var buffer = new ArraySegment<byte>(new byte[4096]);
+            var result = await socket.ReceiveAsync(buffer, CancellationToken.None);
+            return Encoding.UTF8.GetString(buffer.Array, 0, result.Count);
+        }
+    }
+}
---
+++ UDPServerTester/Logging/SocketLoggerProvider.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+
+namespace UDPServerTester.Logging
+{
+    public class SocketLoggerProvider : ILoggerProvider
+    {
+        private readonly Func<string, LogLevel, bool> _filter;
+
+        private static LinkedList<WebSocket> sockets;
+
+        public SocketLoggerProvider(Func<string, LogLevel, bool> filter = null)
+        {
+            sockets = new LinkedList<WebSocket>();
+            _filter = filter;
+        }
+
+        private void LogToAll(string message)
+        {
+            lock (sockets)
+            {
+                foreach (var socket in sockets)
+                {
+                    socket.SendAsync(message).Wait();
+                }
+            }
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new SocketLogger(categoryName, _filter, LogToAll);
+        }
+        internal static async Task AddSocketAsync(WebSocket socket)
+        {
+            lock (sockets)
+            {
+                sockets.AddLast(socket);
+            }
+            try
+            {
+                while (socket.State == WebSocketState.Open)
+                {
+                    var message = await socket.GetMessage();
+                    await socket.SendAsync(message);
+                }
+            }
+            finally
+            {
+                lock (sockets)
+                {
+                    sockets.Remove(socket);
+                }
+            }
+        }
+
+        public void Dispose() { }
+    }
+}

fc555398248631762ad2067cbebb3d4931d4d009
Maksim Makushchenko <maksalmak@gmail.com>
2017-04-12 16:23:17 +0300

    Try catch in reading from tcp

--- SituationCenterBackServer/Extensions/TCPClientExtensions.cs
+++ SituationCenterBackServer/Extensions/TCPClientExtensions.cs
@@ -11,20 +11,25 @@ namespace SituationCenterBackServer.Extensions
     {
         public static async Task<int> ReadPacketAsync(this TcpClient client, byte[] buffer, int offset, int size, CancellationToken token)
         {
-            byte[] header = new byte[2];
-            int readed = await client.GetStream().ReadAsync(header, 0, 2, token)-2;
-            int must = ParseHeader(header[0], header[1]);
-            while (readed != must)
+            try
             {
-                readed += await client.GetStream().ReadAsync(buffer, readed, must - readed, token);
+                byte[] header = new byte[2];
+                int readed = await client.GetStream().ReadAsync(header, 0, 2, token) - 2;
+                int must = ParseHeader(header[0], header[1]);
+                while (readed != must)
+                {
+                    readed += await client.GetStream().ReadAsync(buffer, readed, must - readed, token);
+                }
+                return readed;
             }
-            return readed;
+            catch(Exception ex)
+            {
+                return 0;
+            }
         }
         private static int ParseHeader(byte first, byte second)
         {
             return ((first << 8) | second);
         }
     }
-
-
 }

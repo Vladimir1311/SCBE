626b2d0a7c1b9aecc2674f553f9de07b6dc380e8
Maksim Makushchenko <maksalmak@gmail.com>
2017-07-28 16:32:10 +0300

    Start serialize method info

---
+++ CCF/InvokeMessage.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CCF
+{
+    class InvokeMessage
+    {
+        public Guid MessageId { get; set; }
+        public string MethodName { get; set; }
+        public JToken Args { get; set; }
+    }
+}
--- UDPTester/Program.cs
+++ UDPTester/Program.cs
@@ -3,6 +3,8 @@
 using Castle.DynamicProxy;
 using CCF;
 using Common;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -21,21 +23,37 @@ namespace UDPTester
     {
         static void Main(string[] args)
         {
-            HttpClient cl = new HttpClient();
-            Stopwatch watch = new Stopwatch();
-            watch.Start();
-            cl.DefaultRequestHeaders.Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoibWFrc2FsbWFrQGdtYWlsLmNvbSIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6InVzZXIiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjhiYmYwMDU5LTcyZDEtNGRkYy1hNzQ4LTk3OTk1ZDE4M2Q1MiIsIm5iZiI6MTQ5OTExNjk4MCwiZXhwIjoxNDk5MTE5OTgwLCJpc3MiOiJEZW1vSXNzdWVyIiwiYXVkIjoiRGVtb0F1ZGllbmNlIn0.eTRCevKOdHOsr719Ae_mfbFXMnS3lHMqETbM2uXzOvY");
-            Task[] tasks = new Task[10];
-            for (int i = 0; i < 10; i++)
+            var o = new object[] { "Aha", 123, 123.5 };
+            var serialized = o.Select(O => JToken.FromObject(O)).ToArray();
+            var arrTok = JToken.FromObject(o);
+            var arrReader = new JTokenReader(arrTok);
+            while (arrReader.Read())
             {
-                tasks[i] = cl.GetAsync("http://localhost/api/v1/rooms/roomslist");
-                //cl.GetAsync("http://localhost/api/v1/rooms/roomslist").Wait();
-                //tasks[i] = Task.CompletedTask;
+                Console.WriteLine("---");
+                Console.WriteLine(arrReader.ValueType);
             }
-            Task.WaitAll(tasks);
-            Console.WriteLine(watch.ElapsedMilliseconds);
+            foreach (var item in serialized)
+            {
+                JsonReader reader = new JTokenReader(item);
+                Console.WriteLine(reader.Read());
+                Console.WriteLine(reader.ValueType.Name);
+            }
+
+
+            IMyService service = RemoteWorker<IMyService>.Create("http://127.0.0.1");
+            Console.WriteLine("created");
+            var l = service.GetStringLength("ahahahaha", 54, 12.54);
+            Console.WriteLine("Executed "+ l);
             Console.ReadKey();
             
         }
+
+
+
+    }
+
+    public interface IMyService
+    {
+        int GetStringLength(string str, int value, double lol);
     }
 }

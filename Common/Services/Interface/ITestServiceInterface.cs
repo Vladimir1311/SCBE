d9941381cddf2d52e72a93c42a78b9977aff112f
Maksim Makushchenko <maksalmak@gmail.com>
2017-07-28 07:20:28 +0300

    Start develop CCF again

---
+++ Common/Services/Interface/ITestServiceInterface.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Services.Interface
+{
+    interface ITestServiceInterface
+    {
+        int DoSomeWork(string argument);
+    }
+}

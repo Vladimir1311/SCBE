b940468e945cb4c44c8a7ed21bae7464fdd6b138
Maksim <maksalmak@gmail.com>
2017-09-06 19:29:38 +0300

    update CCF adapter to TCP transporting

--- CCF.IPResolver.Adapter/Extension.cs
+++ CCF.IPResolver.Adapter/Extension.cs
@@ -11,45 +11,15 @@ namespace CCF.IPResolver.Adapter
     {
         public static IApplicationBuilder UseAsServise<T>(this IApplicationBuilder app, string endPoint)
         {
-            try
-            {
-                using (var client = new HttpClient())
-                {
-                    var result = client.GetStringAsync($"http://ipresolver.azurewebsites.net/ip/SetCCFEndPoint?interfaceName={typeof(T).FullName}&url={endPoint}").Result;
-                    if (result == "OK")
-                    {
-                        Console.WriteLine($"Registrate as {typeof(T).FullName} success");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Error");
-                    }
-                }
-            }
-            catch
-            {
-                Console.WriteLine("http request error!!! :(");
-            }
-            finally
-            {
-                app.UseMiddleware<CCFAdapterMiddleware<T>>(endPoint);
-                Console.WriteLine("Use Middleware");
-            }
+            var serviceInvoker = app.ApplicationServices.GetService<T>();
+            CCFServicesManager.RegisterService(serviceInvoker);
             return app;
         }
 
 
         public static IServiceCollection AddCCFService<T>(this IServiceCollection services) where T : class
         {
-            using (var client = new HttpClient())
-            {
-                var result = client.GetStringAsync($"http://ipresolver.azurewebsites.net/ip/GetCCFEndPoint?interfaceName={typeof(T)}").Result;
-                Console.WriteLine($"getted url for {typeof(T)} >> {result}");
-                if (result == "") throw new Exception($"No availabale service for {typeof(T)}");
-                var worker = RemoteWorker.Create<T>(result);
-                services.AddSingleton(worker);
-            }
-
+            services.AddTransient(SP => CCFServicesManager.GetService<T>());
             return services;
         }
     }

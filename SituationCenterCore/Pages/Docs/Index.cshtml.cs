4508c00ea6e709fd693d9c6e2343617f1096d9e7
Maksim <maksalmak@gmail.com>
2017-09-15 14:26:48 +0300

    Added Docs page, for documentation

---
+++ SituationCenterCore/Pages/Docs/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SituationCenter.Shared.Exceptions;
+using System.Reflection;
+using System.ComponentModel;
+
+namespace SituationCenterCore.Pages.Docs
+{
+    public class IndexModel : PageModel
+    {
+        public List<StatusCodePresent> StatusCodes { get; private set; }
+        public void OnGet()
+        {
+            StatusCodes= new List<StatusCodePresent>();
+
+            foreach (StatusCode code in Enum.GetValues(typeof(StatusCode)))
+            {
+                StatusCodes.Add(new StatusCodePresent()
+                {
+                    Code = (int)code,
+                    CSharpName = code.ToString(),
+                    Description = typeof(StatusCode)
+                            .GetMember(code.ToString())[0]
+                            .GetCustomAttribute<DescriptionAttribute>().Description
+                });
+            }
+        }
+
+        public class StatusCodePresent
+        {
+            public int Code { get; set; }
+            public string Description { get; set; }
+            public string CSharpName { get; set; }
+        }
+    }
+}

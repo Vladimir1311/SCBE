e90254bb7865cd3f6a11367afbca6126c26fc843
Maksim <maksalmak@gmail.com>
2017-09-24 03:32:36 +0300

    replace valuetuple to class

--- IPResolver/Models/TCPService.cs
+++ IPResolver/Models/TCPService.cs
@@ -10,26 +10,31 @@ namespace IPResolver.Models
     {
         public HashSet<TCPServiceUser> Listeners { get; set; } = new HashSet<TCPServiceUser>();
 
-
-        private HashSet<(Guid packId, int serviceId, ManualResetEvent msEvent)> servicesIds =
-            new HashSet<(Guid packId, int serviceId, ManualResetEvent msEvent)>();
+        private class CreateInstanceSet
+        {
+            public Guid PackId { get; set; }
+            public int ServiceId { get; set; }
+            public ManualResetEvent MsEvent { get; set; }
+        }
+        private HashSet<CreateInstanceSet> servicesIds =
+            new HashSet<CreateInstanceSet>();
         internal async Task<int> CreateInstanse()
         {
             var msEvent = new ManualResetEvent(false);
-            var tuple = (packId: Guid.NewGuid(), messageId: -1, msEvent);
-            servicesIds.Add(tuple);
-            await SendMessage(17, tuple.packId, CCF.Shared.MessageType.ServiceCreateRequest, null);
+            var set = new CreateInstanceSet { PackId = Guid.NewGuid(), ServiceId = -1, MsEvent = msEvent };
+            servicesIds.Add(set);
+            await SendMessage(17, set.PackId, CCF.Shared.MessageType.ServiceCreateRequest, null);
             msEvent.WaitOne();
-            return tuple.messageId;
+            return set.ServiceId;
         }
 
 
         internal void SetInstaceCreating(Guid id, int serviceId)
         {
-            var tuple = servicesIds.FirstOrDefault(T => T.packId == id);
-            if (tuple.msEvent == null) return;
-            tuple.serviceId = serviceId;
-            tuple.msEvent.Set();
+            var tuple = servicesIds.FirstOrDefault(T => T.PackId == id);
+            if (tuple == null) return;
+            tuple.ServiceId= serviceId;
+            tuple.MsEvent.Set();
         }
     }
 }

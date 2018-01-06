fdc155e1349ff95f7ced840a1106b641230f78f8
Maksim <maksalmak@gmail.com>
2018-01-06 03:14:54 +0300

    Interesting spaces

--- IPResolver/Startup.cs
+++ IPResolver/Startup.cs
@@ -24,9 +24,9 @@ namespace IPResolver
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            string connection = Configuration.GetConnectionString("DefaultConnection");
-            
-            //services.AddDbContext<ServicesContext>(options =>
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            
+            //services.AddDbContext<ServicesContext>(options =>
               //  options.UseSqlServer(connection));
 
             services.AddMvc();
